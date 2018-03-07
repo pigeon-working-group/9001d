c572d6b2854ea6b3f70321e981889f6b0e5ee6c0
Philip Trauner <philip.trauner@arztpraxis.io>
2018-03-07 16:48:07 +0100

    Reworked error detection logic

--- src/gpio_pub/main.rs
+++ src/gpio_pub/main.rs
@@ -1,9 +1,8 @@
 extern crate nanomsg;
 extern crate rand;
 extern crate rppal;
-extern crate structopt;
 #[macro_use]
-extern crate structopt_derive;
+extern crate structopt;
 
 extern crate shared;
 
@@ -13,12 +12,11 @@ use std::time::Duration;
 use nanomsg::{Protocol, Socket};
 
 use rppal::gpio::{Gpio, Level, Mode};
-use rppal::system::DeviceInfo;
 
 use rand::distributions::Range;
 
-use shared::utils::{publish, publish_random_values};
 use shared::types::{str_to_pub_type, PubMessage};
+use shared::utils::{fill_message_integral, publish, publish_random_values};
 
 use structopt::StructOpt;
 
@@ -27,6 +25,9 @@ use structopt::StructOpt;
 struct Opt {
     #[structopt(short = "p", long = "pin")]
     pin: u8,
+
+    #[structopt(short = "d", long = "delay", default_value = "1")]
+    delay: u64,
 
     #[structopt(short = "t", long = "pub-type")]
     pub_type: String,
@@ -41,7 +42,8 @@ fn main() {
     // Obtain config values
     let pin = opt.pin;
     let address = opt.address;
-    let pub_type = str_to_pub_type(&opt.pub_type).expect("invalid publisher type");
+    let pub_type =
+        str_to_pub_type(&opt.pub_type).expect("invalid publisher type");
 
     // Create nanomsg publisher socket
     let mut socket = Socket::new(Protocol::Pub).unwrap();
@@ -50,26 +52,25 @@ fn main() {
     // Instead of creating new messages modify an existing one
     let mut msg = PubMessage {
         pub_type: pub_type,
-        value: 0,
+        integral: 0,
+        decimal: 0.0,
     };
 
-    // std::thread::yield_now() can not be used to prevent excessive CPU
-    // usage -> sleep 1ms instead
-    let sleep_duration = Duration::from_millis(1);
+    let sleep_duration = Duration::from_millis(opt.delay);
 
     // Generate random values if not running on Raspberry Pi
-    if DeviceInfo::new().is_err() {
-        publish_random_values(socket, msg, sleep_duration, Range::new(0, 2));
-    } else {
-        let mut gpio = Gpio::new().expect("could not create Gpio instance");
+    if let Ok(mut gpio) = Gpio::new() {
         gpio.set_mode(pin, Mode::Input);
 
         loop {
             if let Ok(level) = gpio.read(pin) {
-                msg.value = match level {
-                    Level::High => 1,
-                    Level::Low => 0,
-                }
+                msg = fill_message_integral(
+                    match level {
+                        Level::High => 1,
+                        Level::Low => 0,
+                    },
+                    msg,
+                );
             } else {
                 panic!("could not read from Gpio pin")
             }
@@ -78,5 +79,12 @@ fn main() {
 
             sleep(sleep_duration);
         }
+    } else {
+        publish_random_values(
+            socket,
+            msg,
+            sleep_duration,
+            Range::new(0.0, 2.0),
+        );
     }
 }
